eac8648572b39535a1ab12f22e8ce4253ebf827a
Pablo Maceda <pablomacedagarcia@gmail.com>
2019-03-11 00:41:21 +0000

    Update theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,8 +14,8 @@ module.exports = {
         name: 'gatsby-starter-default',
         short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#101010',
+        theme_color: '#033e6b',
         display: 'minimal-ui',
         icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
       },
