2465dc7dafc210a45ddc10b3c40768016a056a8e
Pablo Maceda <pablomacedagarcia@gmail.com>
2019-06-08 19:57:43 +0100

    Use svg icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 export default () => (
   <footer id="footer">
@@ -7,36 +8,39 @@ export default () => (
         <li>
           <a
             href="mailto:contact@pablo.dev"
-            className="icon fa-envelope-o"
+            className="icon"
             rel="noopener noreferrer"
             target="_blank"
             aria-label="Email"
             title="Email"
           >
+            <FontAwesomeIcon icon={["far", "envelope"]} />
             <span className="label">Email</span>
           </a>
         </li>
         <li>
           <a
             href="https://gitlab.com/meuxx"
-            className="icon fa-gitlab"
+            className="icon"
             rel="noopener noreferrer"
             target="_blank"
             aria-label="GitLab"
             title="GitLab"
           >
+            <FontAwesomeIcon icon={["fab", "gitlab"]} />
             <span className="label">GitLab</span>
           </a>
         </li>
         <li>
           <a
             href="https://github.com/meuxx"
-            className="icon fa-github"
+            className="icon"
             rel="noopener noreferrer"
             target="_blank"
             aria-label="GitHub"
             title="GitHub"
           >
+            <FontAwesomeIcon icon={["fab", "github"]} />
             <span className="label">GitHub</span>
           </a>
         </li>
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import '../assets/scss/main.scss'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
+
 import Header from './Header'
+
+
+library.add(fab, faEnvelope)
 
 export default ({ children }) => (
   <div>
