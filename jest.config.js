d82374a193ae4d66d1a2600ce98ca9d774ae435d
Pablo Maceda <pablomacedagarcia@gmail.com>
2020-03-21 20:23:19 +0000

    Replace ts-jest with @babel/preset-typescript

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-    '^.+\\.jsx?$': `<rootDir>/jest-preprocess.js`,
+    '^.+\\.[tj]sx?$': `<rootDir>/jest-preprocess.js`,
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.([tj]sx?)$',
   moduleNameMapper: {
