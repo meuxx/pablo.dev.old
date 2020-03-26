8d843b019240aaf3f6c7fc2ecb2098f5d20292fc
Pablo Maceda <pablomacedagarcia@gmail.com>
2020-03-26 01:26:00 +0000

    Add E2E tests (#354)
    * Add E2E tests
    
    * Add eslint-plugin-cypress
    
    * Ignore cypress folder in jest tests
    
    * Fix env
    
    * Upload screenshot
    
    * Upload artifact as single file
    
    * Upload artifact as single file
    
    * Use Netlify to host screenshots
    
    * Add screenshots per device
    
    * Fix formatting
    
    * Wait for the avatar to be fully loaded
    
    * Fix formatting
    
    * Reduce preview image size

---
+++ cypress/e2e/screenshot.test.js
@@ -0,0 +1,29 @@
+describe('index', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('takes a screenshot', () => {
+    cy.contains('View CV')
+
+    // Wait for the avatar to be fully loaded
+    cy.get('picture')
+      .find('img')
+      .should('be.visible')
+      .and($img => {
+        expect($img[0].naturalWidth).to.be.greaterThan(0)
+      })
+
+    cy.viewport('macbook-13')
+    cy.screenshot('preview', { clip: { x: 0, y: 0, width: 1366, height: 400 } })
+
+    cy.viewport('macbook-13')
+    cy.screenshot('macbook-13')
+
+    cy.viewport('ipad-mini')
+    cy.screenshot('ipad-mini')
+
+    cy.viewport('iphone-x')
+    cy.screenshot('iphone-x')
+  })
+})
