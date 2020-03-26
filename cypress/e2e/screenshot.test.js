5c35943d6097f5d1429945adc0753c39ae8237c8
Pablo Maceda <pablomacedagarcia@gmail.com>
2020-03-26 00:31:10 +0000

    Add screenshots per device

--- cypress/e2e/screenshot.test.js
+++ cypress/e2e/screenshot.test.js
@@ -5,6 +5,17 @@ describe('index', () => {
 
   it('takes a screenshot', () => {
     cy.contains('View CV')
-    cy.screenshot('index')
+
+    cy.viewport('macbook-13')
+    cy.screenshot('macbook-13')
+
+    cy.viewport('ipad-mini')
+    cy.screenshot('ipad-mini')
+
+    cy.viewport('iphone-x')
+    cy.screenshot('iphone-x')
+
+    cy.viewport('macbook-13')
+    cy.screenshot('cropped', { clip: { x: 0, y: 0, width: 1366, height: 500 } })
   })
 })
