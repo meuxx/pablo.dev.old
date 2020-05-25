5f74822ad6bde99b34d7e2754a20d7e33454f454
Pablo Maceda <pablomacedagarcia@gmail.com>
2020-05-25 23:10:29 +0100

    Improve link styles and update netlify url (#656)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import Avatar from './Avatar'
 import Footer from './Footer'
-import { Sidebar, SidebarMain, SidebarTitle, SidebarSubtitle, SidebarLink } from '../styles/styles'
+import { Sidebar, SidebarMain, SidebarTitle, SidebarSubtitle, Link } from '../styles/styles'
 import data from '../data.json'
 
 const getShortUrl = (url: string) => {
@@ -18,7 +18,7 @@ const Header: React.FC = () => (
       <SidebarTitle>
         <strong>{data.site.author}</strong>
         <br />{' '}
-        <SidebarLink
+        <Link
           href={`mailto:${data.author.email}`}
           rel="noopener noreferrer"
           target="_blank"
@@ -26,21 +26,21 @@ const Header: React.FC = () => (
           title="Contact email"
         >
           {data.author.email}
-        </SidebarLink>
+        </Link>
       </SidebarTitle>
 
       <br />
 
       <SidebarSubtitle>
         {data.author.jobTitle} at{' '}
-        <SidebarLink
+        <Link
           href={data.author.organizationUrl}
           rel="noopener noreferrer"
           target="_blank"
           title={data.author.organizationName}
         >
           {getShortUrl(data.author.organizationUrl)}
-        </SidebarLink>
+        </Link>
         <br />
         {`${data.author.city}, ${data.author.country}`}
       </SidebarSubtitle>
