11e9bfa309f9c72e0de2fe421218ebce214e1b6f
Pablo Maceda <pablomacedagarcia@gmail.com>
2020-05-25 23:10:16 +0100

    Add 512x512 PWA icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,6 +59,7 @@ module.exports = {
           { src: 'icons/icon-96x96.png', sizes: '96x96', type: 'image/png' },
           { src: 'icons/icon-144x144.png', sizes: '144x144', type: 'image/png' },
           { src: 'icons/icon-192x192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'icons/icon-512x512.png', sizes: '512x512', type: 'image/png' },
         ],
         lang: `en-GB`,
         cache_busting_mode: 'none',
